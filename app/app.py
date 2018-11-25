bd40f253e53f0fe2446b2d0589078b0df236a0c1
jpaulini <jpaulini@gmail.com>
2018-11-25 12:09:47 -0300

    renombres y cosas provisorias menores

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
+#!/bin/python
+# -*- coding: utf-8 -*- 
 import os
 import json
 
 from flask import Flask, jsonify
-from flask_restful import reqparse, abort, Api, Resource, fields, marshal_with
 
 execution_path = os.getcwd()
 models_dir = os.path.join(execution_path, "models")
 
 app = Flask(__name__)
-api = Api(app)
 
 # argument parsing
 # Estos fueron los argumentos acordados al 2018-11-21 en mail de Seba Santos
@@ -20,32 +20,9 @@ api = Api(app)
                                "y": -32.7657464119999 }
 }
 """
-
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-class GetEESCapture(Resource):
-    def __init__(self, eeslist = None ):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('simulacion', type = list, location = 'json')
-        self.parser.add_argument('cluster', type= list, location = 'json')
-
-    def get(self):
-        return jsonify(self.ees_list)
-    
-    def post(self):
-        args = self.parser.parse_args()
-        print(args["simulacion"])
-        print(args["cluster"])
-        return None
-
-# Setup the Api resource routing here
-# Route the URL to the resource
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(GetEESCapture, '/eesSim')
+@app.route('/')
+def im_alive():
+    return jsonify({ "message": "I'm alive!") 
 
 if __name__ == '__main__':
     app.run(debug=True)
